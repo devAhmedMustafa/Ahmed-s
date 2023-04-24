75098bc9bd5ffeae31d15adfb0598b5ad79436e5
Ahmed Mustafa <am1804067@gmail.com>
2023-04-24 03:59:12 +0200

    vercel deploy

--- main_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Message, Skill, SkillCategory
-# Register your models here.
-
-admin.site.register(Message)
-admin.site.register(Skill)
-admin.site.register(SkillCategory)
--- main_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('send_msg/', views.send_message, name='send_msg'),
-    path('skills/', views.skills, name='skills'),
-    path('projects/', views.projects, name='projects')
-]
